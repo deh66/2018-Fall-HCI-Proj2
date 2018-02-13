ad5c39a1b842e134f139f303f2136cd2481f6a41
Daniel Hollo <deh66@zips.uakron.edu>
2018-02-13 16:16:15 -0500

    Added actual source file Program.cs that is used by the IDE (Visual Studio).

---
+++ HCI Project 2/HCI Project 2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HCI_Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s;
+            int a;
+
+            double pi = 3.14159265358979323846264338;
+
+            Console.WriteLine("Enter how many iterations you want executed (integer): (or q to quit)");
+
+            s = Console.ReadLine();
+
+            // Main Loop
+            while (s != "q" && s != "quit")
+            {
+                // Initilize Loop Variables
+                int count = 0;
+                double ans = pi;
+
+
+                // Parse input to first integer
+                while (!Int32.TryParse(s, out a))
+                {
+                    Console.WriteLine("Invalid Input: Only enter an Integer Value: ");
+                    s = Console.ReadLine();
+                }
+
+
+                // Loop until requested iterations reached
+                while (count < a)
+                {
+                    // Call Compute Function
+                    ans = Compute(ans);
+                    // Increment counter
+                    count++;
+                    // Output iteration
+                    Console.WriteLine($"Iteration: {count}");
+                }
+
+                Console.WriteLine($"Result of Compute = {ans}");
+
+
+                Console.WriteLine("Enter an Integer: (or q to exit)");
+                s = Console.ReadLine();
+            }
+        }
+
+        static private double Compute(double a)
+        {
+            double result = 0;
+            // Computes Square Root
+            result = Math.Sqrt(a);
+
+            return result;
+        }
+
+    }
+}
